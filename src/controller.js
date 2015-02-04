763f3f204bdbf17009d6b5bcd412dbd04047fc97
lmikulin <lmikulin@gmail.com>
2015-02-03 23:13:38 -0700

    Added Removal and Slide

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,10 @@
 //Function run when a direction button is pressed
 function direction(heading){
-	var txt = $("<pre></pre>").text(" " + heading);   // Create output with jQuery
+	var txt = $("<pre class='item'></pre>").text(" " + heading);   // Create output with jQuery
 	$("#textTarget").prepend(txt);         // Prepend the new element in the screen after #textTarget
+	$(".item:gt(10)").css("color","gray");
+	$(".item:gt(15)").css("color","lightgray");
+	$(".item:gt(20)").slideUp("slow", function(){
+		$(".item:gt(21)").remove();
+	});
 }
